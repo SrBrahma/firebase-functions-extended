687ad0c0cf900ab4d97d9b101b006c60c28b7b98
Henrique Bruno <henrique.bruno.fa@gmail.com>
2020-08-23 15:42:17 -0300

    throwError to ExtError, allowing 'throw ExtError'

--- src/caller.ts
+++ src/caller.ts
@@ -1,5 +1,6 @@
 // Caller -> The one calling the function.
 import { https } from 'firebase-functions';
+import admin from 'firebase-admin';
 
 
 // Properties that apply to Caller and User, must be in User (like userId).
@@ -14,16 +15,16 @@ export class Caller {
 
   // Will, by default, always return true as Cloud Functions callers must be authed, unless
   // you change it on settings somewhere.
-  get isAuthed() { return !!this.context.auth; }
+  get isAuthed(): boolean { return !!this.context.auth; }
 
-  get token() { return this.context.auth?.token; }
+  get token(): admin.auth.DecodedIdToken | undefined { return this.context.auth?.token; }
 
   // [Google, facebook, "anonymous"] etc. If wanted, add here the exact strings of the others.
-  get provider() { return this.token?.firebase.sign_in_provider; }
+  get provider(): any { return this.token?.firebase.sign_in_provider; }
 
   // https://stackoverflow.com/a/62607358/10247962
-  get isAnonymous() { return this.provider === 'anonymous'; }
+  get isAnonymous(): boolean { return this.provider === 'anonymous'; }
 
-  // If you allow not authed cloud function calls, check if it is undefined before, or check isAuthed before.
-  get uid() { return this.context.auth?.uid as string; }
+  // If you allow not authed cloud function calls, check if it is '' before, or check isAuthed before.
+  get uid(): string { return this.context.auth?.uid || ''; }
 }
--- src/extCall.ts
+++ src/extCall.ts
@@ -10,13 +10,15 @@ import { ZodRawShape } from 'zod/lib/src/types/base';
 // https://firebase.google.com/docs/functions/locations#best_practices_for_changing_region
 let region = 'southamerica-east1';
 
+type onCallRtn = ReturnType<typeof functions.https.onCall>;
+
 
 // Should be called before defining your extCalls.
-export function setExtCallDefaultRegion(regionId: string) { region = regionId; }
+export function setExtCallDefaultRegion(regionId: string): void { region = regionId; }
 
 
 // ONLY adds 'caller' var and uses the set region. Maybe you will want to use it.
-export function onCallWithCaller(handler: (data: any, caller: Caller) => any | Promise<any>) {
+export function onCallWithCaller(handler: (data: any, caller: Caller) => any | Promise<any>): onCallRtn {
   // Changing region. Used it only here and not directly on functions on ./firebase.ts,
   // Because this doc says background functions with Realtime DB as Trigger should use
   // the default region ('use-central1').
@@ -27,24 +29,24 @@ export function onCallWithCaller(handler: (data: any, caller: Caller) => any | P
 
 
 // throwError code is optional. Does anyone even uses it?
-type ExtCallHandler<T extends ZodRawShape> = {
+type Handler<T extends ZodRawShape> = {
   data: zod.infer<zod.ZodObject<T>>,
   caller: Caller;
-  throwError: (message: string, code?: functions.https.FunctionsErrorCode) => any;
+  ExtError: (message: string, code?: functions.https.FunctionsErrorCode) => any;
 };
 
 
 
 type ExtCall<T extends ZodRawShape> = {
   zodObj: T,
-  handler: (args: ExtCallHandler<T>) => any | Promise<any>,
+  handler: (args: Handler<T>) => any | Promise<any>,
   allowAnonymous?: true; // Throws error if false and if caller is anonymous
 };
 
 
 
 // TODO: add obj to message type, to allow languages given by the caller.
-function throwError(
+function InternalExtError(
   message: string, // | ExtError
   code: functions.https.FunctionsErrorCode,
   data: any,
@@ -52,13 +54,13 @@ function throwError(
 ) {
   // _callerToken with _ to keep it on the end of the json for better readibility on firebase console
   console.error(new Error(JSON.stringify({ code, data, message, _callerToken: caller.token })));
-  throw new functions.https.HttpsError(code, message);
+  return new functions.https.HttpsError(code, message);
 }
 
 
 // TODO: Optional zodObj. Won't use now, so leaving it for later
 // TODO: Take the language from data (if there is) and pass it to the caller
-export function extCall<T extends ZodRawShape>({ zodObj, handler, allowAnonymous, }: ExtCall<T>) {
+export function extCall<T extends ZodRawShape>({ zodObj, handler, allowAnonymous }: ExtCall<T>): onCallRtn {
 
   return functions.region(region).https.onCall(async (data, context) => {
     // TODO: wrapping try/catch
@@ -66,7 +68,7 @@ export function extCall<T extends ZodRawShape>({ zodObj, handler, allowAnonymous
     const caller = new Caller(context);
 
     if (!allowAnonymous && caller.isAnonymous)
-      throwError('Usuário não pode ser anônimo.', 'unauthenticated', data, caller);
+      throw InternalExtError('Usuário não pode ser anônimo.', 'unauthenticated', data, caller);
 
 
     // TODO: add support for zod invalid schema message
@@ -74,10 +76,10 @@ export function extCall<T extends ZodRawShape>({ zodObj, handler, allowAnonymous
       await handler({
         data,
         caller,
-        throwError: (message, code = 'internal') => throwError(message, code, data, caller)
+        ExtError: (message, code = 'internal') => InternalExtError(message, code, data, caller)
       });
     }
     else
-      throwError('Argumentos inválidos', 'invalid-argument', data, caller);
+      throw InternalExtError('Argumentos inválidos', 'invalid-argument', data, caller);
   });
 }
