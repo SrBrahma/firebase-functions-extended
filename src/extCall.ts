e5031b86079f1cbf8825f3c11bf9ce8f4dbf5282
Henrique Bruno <henrique.bruno.fa@gmail.com>
2020-09-25 04:55:55 -0300

    added region property to extCall param

--- src/extCall.ts
+++ src/extCall.ts
@@ -7,26 +7,34 @@ import { ZodRawShape } from 'zod/lib/src/types/base';
 import { debug, info, error, warn } from 'firebase-functions/lib/logger';
 
 
-// Changing region. Used it only here and not directly on functions on ./firebase.ts,
-// Because this doc says background functions with Realtime DB as Trigger should use
-// the default region ('use-central1').
+
 // https://firebase.google.com/docs/functions/locations#best_practices_for_changing_region
-let region = 'southamerica-east1';
+// Default Firebase value. It's the best for Realtime Database, as it always uses us-central1
+const constDefaultRegion = 'us-central1';
+let defaultRegion = constDefaultRegion;
 
 type onCallRtn = ReturnType<typeof functions.https.onCall>;
 
 
-// Should be called before defining your extCalls.
-export function setExtCallDefaultRegion(regionId: string): void { region = regionId; }
+/**
+ * Should be called before defining your extCalls.
+ *
+ * You can also specify a region for a specific function by using the
+ * `region` property in the extCall argument.
+ *
+ * Read https://firebase.google.com/docs/functions/locations#best_practices_for_changing_region
+ * @param regionId - default is `'us-central1'`
+ */
+export function setExtCallDefaultRegion(regionId: string = constDefaultRegion): void { defaultRegion = regionId; }
 
 
-// ONLY adds 'caller' var and uses the set region. Maybe you will want to use it.
+/** ONLY adds 'caller' var and uses the set region. Maybe you will want to use it.  */
 export function onCallWithCaller(handler: (data: any, caller: Caller) => any | Promise<any>): onCallRtn {
   // Changing region. Used it only here and not directly on functions on ./firebase.ts,
   // Because this doc says background functions with Realtime DB as Trigger should use
   // the default region ('use-central1').
   // https://firebase.google.com/docs/functions/locations#best_practices_for_changing_region
-  return functions.region(region).https.onCall((data, context) =>
+  return functions.region(defaultRegion).https.onCall((data, context) =>
     handler(data, new Caller(context)));
 }
 
@@ -43,7 +51,10 @@ type Handler<T extends ZodRawShape> = {
 type ExtCall<T extends ZodRawShape> = {
   zodObj: T,
   handler: (args: Handler<T>) => any | Promise<any>,
-  allowAnonymous?: true; // Throws error if false and if caller is anonymous
+  /** Throws error if false and if caller is anonymous */
+  allowAnonymous?: true;
+  /** You can specify a region diferent of the default one (`us-central1` or the one set by `setExtCallDefaultRegion()`) */
+  region?: string;
 };
 
 
@@ -66,9 +77,9 @@ function InternalExtError(
 
 // TODO: Optional zodObj. Won't use now, so leaving it for later
 // TODO: Take the language from data (if there is) and pass it to the caller
-export function extCall<T extends ZodRawShape>({ zodObj, handler, allowAnonymous }: ExtCall<T>): onCallRtn {
+export function extCall<T extends ZodRawShape>({ zodObj, handler, allowAnonymous, region }: ExtCall<T>): onCallRtn {
 
-  return functions.region(region).https.onCall(async (data, context) => {
+  return functions.region(region || defaultRegion).https.onCall(async (data, context) => {
     // TODO: wrapping try/catch
     const schema = zod.object(zodObj);
     const caller = new Caller(context);
