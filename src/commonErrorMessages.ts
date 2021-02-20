d479437f0d56e2ef62032a7bbf259f13c5850713
Henrique Bruno <henrique.bruno.fa@gmail.com>
2021-02-20 01:21:03 -0300

    ptbr to just pt

--- src/commonErrorMessages.ts
+++ src/commonErrorMessages.ts
@@ -14,7 +14,7 @@ export enum CommonErrorsId {
 
 export enum CommonErrorsDefaultLanguages {
   en = 'en',
-  ptbr = 'ptbr'
+  pt = 'pt'
 }
 
 // TODO add a package identification to those errors, but still user friendly.
@@ -22,24 +22,24 @@ export enum CommonErrorsDefaultLanguages {
  * caller is anon, invalid arguments (zod didn't accept it) or unexpected error / not caught.
  *
  * If you want to provide your own messages for them and or want to include other languages besides
- * the current `en` and `ptbr`, call
+ * the current `en` and `pt`, call
  */
 export const defaultCommonErrorMessagesDict: ErrorsMessagesDict<CommonErrorsId, CommonErrorsDefaultLanguages> = {
   authRequired: {
     en: 'You must be logged in to perform this action.',
-    ptbr: 'Você precisa estar logado para executar esta ação.',
+    pt: 'Você precisa estar logado para executar esta ação.',
   },
   cantBeAnon: {
     en: 'You must be properly logged in to perform this action.',
-    ptbr: 'Você precisa estar devidamente logado para executar esta ação.',
+    pt: 'Você precisa estar devidamente logado para executar esta ação.',
   },
   invalidArgs: {
     en: 'Invalid arguments.',
-    ptbr: 'Argumentos inválidos.',
+    pt: 'Argumentos inválidos.',
   },
   unknown: {
     en: 'An error occurred in the server, try again.',
-    ptbr: 'Ocorreu um erro no servidor, tente novamente.',
+    pt: 'Ocorreu um erro no servidor, tente novamente.',
   }
 };
 
@@ -48,7 +48,7 @@ export let commonErrorMessages = defaultCommonErrorMessagesDict;
 
 /** If you want to provide your own messages for common errors like invalid arguments,
  * user not authed etc and/or want to include other languages besides
- * the current `en` and `ptbr`, call this function with your own dictionary before everything.
+ * the current `en` and `pt`, call this function with your own dictionary before everything.
  *
  * @default - the default dictionary. No need to call this function to set it if not changed. */
 export function setCommonErrorMessagesDict(
